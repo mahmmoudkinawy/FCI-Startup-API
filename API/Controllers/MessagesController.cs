efecc3f90e734acb2a7cca9f739895c67885520c
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2023-04-16 21:05:18 +0200

    Added message entity to db and added create a message end point

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -15,10 +15,29 @@ public sealed class MessagesController : ControllerBase
             throw new ArgumentNullException(nameof(mediator));
     }
 
+    /// <summary>
+    /// endpoint for creating a message.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Returns the create message.</returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     POST /messages/6fafacd7-80fa-48c2-9dff-f12c01aa25ed
+    ///     {
+    ///       "content": "hello there"
+    ///     }
+    /// </remarks>
+    /// <response code="200">Returns the created message.</response>
+    /// <response code="400">There exist validation errors.</response>
+    /// <response code="401">User does not exist.</response>
+    /// <response code="404">User with the given ID does not exist.</response>
     [HttpPost("{recipientId:guid}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> CreateMessage(
         [FromBody] CreateMessageProcess.Request request,
         CancellationToken cancellationToken)
